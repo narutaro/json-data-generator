04cf775d56e275dd9840845cdff099152dc57a6a
narutaro <narusan@mac.com>
2024-01-23 18:54:37 +0900

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -1,7 +1,8 @@
+ENDPOINT=$(aws iot describe-endpoint --endpoint-type iot:Data-ATS --output text --query 'endpointAddress')
 mosquitto_pub --cafile AmazonRootCA1.pem \
   --cert device.pem.crt \
   --key private.pem.key \
-  -h <end-point-id>-ats.iot.<region>.amazonaws.com \
+  -h $ENDPOINT \
   -p 8883 \
   -t telemetry \
   -i pub_926b2528 \
